89e453ed63c76731f39a4ba159a0d22d63d36c19
Ming-Zi <1127abj@gmail.com>
2021-03-18 00:30:21 +0900

    git commit

--- chapter_6/6.5.py
+++ chapter_6/6.5.py
@@ -1,4 +1,38 @@
 #BOARDCOVER
+
+basicShape = [
+[[1,1],
+[1,0]],
+[[1,1],
+[0,1]],
+[[1,0],
+[1,1]],
+[[0,1],
+[1,1]]]
+
+setting_1 = [
+[1,0,0,0,0,0,1],
+[1,0,0,0,0,0,1],
+[1,1,0,0,0,1,1]
+]
+
+setting_2 = [
+[1,0,0,0,0,0,1],
+[1,0,0,0,0,0,1],
+[1,1,0,0,1,1,1]
+]
+
+setting_3 = [
+[1,1,1,1,1,1,1,1,1,1],
+[1,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,0,0,0,1],
+[1,1,1,1,1,1,1,1,1,1]
+]
+
 def setGame():
     global setting_1
     global setting_2
@@ -26,36 +60,61 @@ def setGame():
         [1,1,1,1,1,1,1,1,1,1]
     ]
 
-basicShape = [
-[[1,1],
-[1,0]],
-[[1,1],
-[0,1]],
-[[1,0],
-[1,1]],
-[[0,1],
-[1,1]]]
+def shapePlus(setting, i, j, index, select):
+    if (select == 3):
+        j=j-1
+    setting[i][j]+=basicShape[select][0][0]
+    setting[i][j+1]+=basicShape[select][0][1]
+    setting[i+1][j]+=basicShape[select][1][0]
+    setting[i+1][j+1]+=basicShape[select][1][1]
+    if (setting[i+1][j] == 2 or setting[i][j+1] == 2 or setting[i+1][j+1] == 2):
+        setting[i][j]-=basicShape[select][0][0]
+        setting[i][j+1]-=basicShape[select][0][1]
+        setting[i+1][j]-=basicShape[select][1][0]
+        setting[i+1][j+1]-=basicShape[select][1][1]
+        return False
+    return True
 
-
-def game(setting):
-    sum = 0
+def game(gamenum, setting, way):
     row = len(setting)
     col = len(setting[0])
+    sum = row * col
+    rsum = 0
+    
+    # divide with 3 => check
     for i in range(0,row):
         for j in range(0,col):
-            sum = sum + setting[i][j]
+            sum = sum - setting[i][j]
     if (sum%3 != 0):
-        return False
+        return 0
+    
+    # fill the shpae
     for i in range(0, row-1):
         for j in range(0, col-1):
-            if (setting[i][j] == 1):
-                setting[i][j] = setting[i][j] + basicShape[3]
-                if (setting[i][j+1])
+            if (setting[i][j] == 0):
+                for k in range(0,4):
+                    shapePlus(setting, i, j, gamenum, k)
+    
+    # shape complete => return way
+    for i in range(0,row):
+        for j in range(0,col):
+            rsum = rsum + setting[i][j]
+    if (rsum == (row * col)):
+        way = way + 1
+    
+    while True:
+        if ():
+            break
+        else:
+            game(gamenum, setting, way)
+        break
+    return way
 
+setGame()
+print(game(1, setting_1, 0))
 
-set()
-game(setting_1)
-set()
-game(setting_2)
-set()
-game(setting_3)
+setGame()
+print(game(2, setting_2, 0))
+
+setGame()
+print(game(3, setting_3, 0))
